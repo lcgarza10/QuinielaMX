06cfcad038384d19f24d42c43c81929b5f39363a
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-11-09 16:14:34 -0600

    fixed refresh issue

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -8,7 +8,7 @@ export class VersionService {
   private readonly version: string;
 
   constructor() {
-    this.version = environment.production ? '0.9.47' : '0.9.47-dev';
+    this.version = environment.production ? '0.9.48' : '0.9.48-dev';
   }
 
   getVersion(): string {
