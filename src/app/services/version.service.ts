fc5cad1e3f0974b0eaa88773d200ad4bd72e9e88
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-11-10 14:39:36 -0600

    .v56 arreglo de tamaño de tablas para movil

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -8,7 +8,7 @@ export class VersionService {
   private readonly version: string;
 
   constructor() {
-    this.version = environment.production ? '0.9.55' : '0.9.55-dev';
+    this.version = environment.production ? '0.9.56' : '0.9.56-dev';
   }
 
   getVersion(): string {
