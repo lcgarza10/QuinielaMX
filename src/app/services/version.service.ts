258219cb5f8fe7862fb1020b949f2133eccafb82
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-11-10 23:01:23 -0600

    v62

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -8,7 +8,7 @@ export class VersionService {
   private readonly version: string;
 
   constructor() {
-    this.version = environment.production ? '0.9.61' : '0.9.61-dev';
+    this.version = environment.production ? '0.9.62' : '0.9.62-dev';
   }
 
   getVersion(): string {
