061bbab0da67841f9ddacd9e8ee9a82b7eacd60e
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-11-12 14:45:13 -0600

    v.70 Added Capacitor for mobile app and configure build for iOS Android

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -8,7 +8,7 @@ export class VersionService {
   private readonly version: string;
 
   constructor() {
-    this.version = environment.production ? '0.9.63' : '0.9.63-dev';
+    this.version = environment.production ? '0.9.70' : '0.9.70-dev';
   }
 
   getVersion(): string {
