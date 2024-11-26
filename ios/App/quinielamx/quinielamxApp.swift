2ff5bc483e0faa66af661689c0332a0d94d79196
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-11-26 08:20:43 -0600

    Inlcuida la Liguilla V80C

---
+++ ios/App/quinielamx/quinielamxApp.swift
@@ -0,0 +1,17 @@
+//
+//  quinielamxApp.swift
+//  quinielamx
+//
+//  Created by Carlos Garza on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct quinielamxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/App/quinielamxTests/quinielamxTests.swift
@@ -0,0 +1,17 @@
+//
+//  quinielamxTests.swift
+//  quinielamxTests
+//
+//  Created by Carlos Garza on 11/12/24.
+//
+
+import Testing
+@testable import quinielamx
+
+struct quinielamxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
