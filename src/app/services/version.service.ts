1e7a2ed040e156ef40c6ad3420db45b6b645b58a
lcgarza10 <81598405+lcgarza10@users.noreply.github.com>
2024-12-11 22:26:30 -0600

    v1.36

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -8,7 +8,7 @@ export class VersionService {
   private readonly version: string;
 
   constructor() {
-    this.version = environment.production ? '1.35' : '1.35-dev';
+    this.version = environment.production ? '1.36' : '1.36-dev';
   }
 
   getVersion(): string {
